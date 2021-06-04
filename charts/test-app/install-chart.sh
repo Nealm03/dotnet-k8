fdd9de73132280b180213ddb02bfcf0b598d5d2f
Neal Morris <neal.morris@snyk.io>
2021-06-04 20:38:38 +0100

    Add helm script

--- charts/test-app/install-chart.sh
+++ charts/test-app/install-chart.sh
@@ -1,1 +1,16 @@
-helm upgrade --install my-test-app-release .
+#!/bin/bash
+[ -z "$CHART" ] && echo "Missing CHART var" && exit 1;
+[ -z "$RELEASE_NAME" ] && echo "Missing RELEASE_NAME var" && exit 1;
+[ -z "$NAMESPACE" ] && echo "Missing NAMESPACE var" && exit 1;
+
+KUBE_CONTEXT=$(kubectl config current-context)
+
+helm upgrade --install \
+  $RELEASE_NAME \
+  $CHART \
+  --kube-context "${KUBE_CONTEXT}" \
+  --namespace="$NAMESPACE" \
+  $HELM_ARGS
+
+
+# helm upgrade --install my-test-app-release .
